5cf1c715ec38bd6ca14ab09939b783cf679c2d8a
Kevin <kevin@Kevins-MacBook-Pro.local>
2014-11-19 11:57:32 -0800

    Add quotes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def home
+    @quotes = ["\"Failure is not an option. Everyone has to succeed.\" - Arnold Schwarzenegger", "\"Your time is limited, so don't waste it living someone else's life.\" - Steve Jobs", "\"Better ingredients, better pizza\" - Papa John"]
   end
 
   def about
