79691df24cbf0e5de06353a6a1d2bd7c008e45ab
Kevin <kevin@Kevins-MacBook-Pro.local>
2014-11-20 11:53:03 -0800

    Update display of quotes and their authors

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
   def home
-    @quotes = ["\"Failure is not an option. Everyone has to succeed.\" - Arnold Schwarzenegger", "\"Your time is limited, so don't waste it living someone else's life.\" - Steve Jobs", "\"Better ingredients, better pizza\" - Papa John"]
+    @quotes = [
+      ["\"Failure is not an option. Everyone has to succeed.\"", "Arnold Schwarzenegger",],
+      ["\"Your time is limited, so don't waste it living someone else's life.\"", "Steve Jobs",],
+      ["\"Better ingredients, better pizza\"", "Papa John",],
+    ]
   end
 
   def about
